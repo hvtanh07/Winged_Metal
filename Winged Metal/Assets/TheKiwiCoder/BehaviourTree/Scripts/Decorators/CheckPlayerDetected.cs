bde0b6f8eeebb7341f0b07278a89fe16e6146ae0
Tuấn Anh <hvtanh07@gmail.com>
2023-09-18 10:09:44 +0700

    changing Decorator name to CheckPlayerDetected

---
+++ Winged Metal/Assets/TheKiwiCoder/BehaviourTree/Scripts/Decorators/CheckPlayerDetected.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class CheckPlayerDetected : DecoratorNode
+{
+    protected override void OnStart()
+    {
+
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        if ((blackboard.playerPos.position - context.transform.position).magnitude <= blackboard.detectingDistance &&
+            !Physics2D.Linecast(blackboard.playerPos.position,context.transform.position, blackboard.viewBlock))
+        {
+            var state = child.Update();
+            return state;
+        }
+        return State.Failure;
+    }
+}
+
