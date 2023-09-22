8f69e2d7b438094c621d49e5edb93bc343b70a4e
Tuấn Anh <hvtanh07@gmail.com>
2023-09-22 15:00:50 +0700

    fix BT detecting dash & energy to use at the same time

--- Winged Metal/Assets/TheKiwiCoder/BehaviourTree/Scripts/Decorators/CheckBeingFired.cs
+++ Winged Metal/Assets/TheKiwiCoder/BehaviourTree/Scripts/Decorators/CheckBeingFired.cs
@@ -7,16 +7,21 @@ public class CheckBeingFired : DecoratorNode
 {
     public float projectileRadarRadius;
     public LayerMask projectile;
-    private Collider2D hit;
-    protected override void OnStart() {
-        
+    protected override void OnStart()
+    {
+
     }
 
-    protected override void OnStop() {
+    protected override void OnStop()
+    {
     }
 
-    protected override State OnUpdate() {
-        if(Physics2D.OverlapCircle(context.transform.position,projectileRadarRadius,projectile).gameObject.GetComponent<BulletScript>().bulletOwner == TankAttack.BulletOwner.player){
+    protected override State OnUpdate()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(context.transform.position, projectileRadarRadius, projectile);
+        if (hit == null) return State.Success;
+        if (hit.gameObject.GetComponent<BulletScript>().bulletOwner == TankAttack.BulletOwner.player)
+        {
             var state = child.Update();
             return state;
         }
