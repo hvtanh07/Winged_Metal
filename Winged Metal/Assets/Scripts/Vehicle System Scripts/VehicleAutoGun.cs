990d603ba135f7b6e4640ec452dfc0ed86fc681f
Tuấn Anh <hvtanh07@gmail.com>
2023-11-06 17:01:05 +0700

    change Vehicle System path name

--- Winged Metal/Assets/Scripts/Vehicle System Scripts/VehicleAutoGun.cs
+++ Winged Metal/Assets/Scripts/Vehicle System Scripts/VehicleAutoGun.cs
@@ -11,7 +11,6 @@ public class VehicleAutoGun : VehicleSystem
     public LayerMask obstacle;
     [Space]
     [Header("Autogun attack")]
-    public int enConsum;
     public Transform shootingPoint;
     public int damage;
     public float fireRate;
